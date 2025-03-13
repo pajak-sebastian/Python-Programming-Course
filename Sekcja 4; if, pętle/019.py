a961674f25b031eb8eecdae79f53f0a7a858fa97
pajak-sebastian <pajak.sebastian12@gmail.com>
2025-03-13 17:18:29 +0100

    zadanie if elif else

--- Sekcja 4; if, pętle/019.py
+++ Sekcja 4; if, pętle/019.py
@@ -1,2 +1,23 @@
-a = 1+2
-print("a")
+num = 15
+
+if num > 10:
+    print(f"{num} > 10")
+    
+if num == 15:
+    print(f"{num} == 15")
+
+elif num == 20:
+    print(f"{num} == 20")
+
+elif num == 25:
+    print(f"{num} == 25")
+
+elif num > 30:
+    print(f"{num} > 30")
+
+else:
+    print("Domyslny komunikat")
+
+if num > 10:
+    if num % 3 == 0:
+        print("Liczba jest podzielna przez 3") 
