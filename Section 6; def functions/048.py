77dbd6036c091eacb450f2375056783a3422cb87
pajak-sebastian <pajak.sebastian12@gmail.com>
2025-03-26 19:34:16 +0100

    return, excercise

---
+++ Section 6; def functions/048.py
@@ -0,0 +1,20 @@
+def createUser(name, contact):
+    user = {
+        "name" : name,
+        "email" : None,
+        "telephone" : None
+    }
+
+    if isinstance(contact, str):
+        user["email"] = contact
+    
+    elif isinstance(contact, int):
+        user["telephone"] = contact
+
+    return user
+
+user1 = createUser("Ola" , "ola@example.com")
+print(user1)
+
+user2 = createUser("Kasia" , 987654321)
+print(user2)
