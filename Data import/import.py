56c7c2f00993b10ab528e4ab1bbd35e80322cb9e
pajak-sebastian <pajak.sebastian12@gmail.com>
2025-04-30 18:00:31 +0200

    docx range 1.0

--- Data import/import.py
+++ Data import/import.py
@@ -2,24 +2,35 @@
 #pip install python-docx
 #92px Arial
 #dodać funkcjonalność która ogranicza dodanie zakresu ponad 100 liczb
-
+import sys
 from docx import Document
 from docx.shared import Pt
 
 def number_print_function():
-    print("Podaj zakres (maksymalnie do 100)")
+    try:
+        print("Podaj zakres (maksymalnie do 100)")
 
-    start = int(input("Od: "))
-    end = int(input("Do: "))
-    
-    numList = ' '.join(str(i) for i in range(start, end + 1))
+        start = int(input("Od: "))
+        end = int(input("Do: "))
+        
+        numList = ' '.join(str(i) for i in range(start, end + 1))
 
-    document = Document()
-    style = document.styles["Normal"]
-    font = style.font
-    font.name = 'Arial'
-    font.size = Pt(91)
-    document.add_paragraph(numList)
-    document.save("Drukowanie numerków.docx")
+        if (end - start > 100):
+            print("Wyszedłeś poza zakres, spróbuj ponownie.")
+            exit_program()
+
+        document = Document()
+        style = document.styles["Normal"]
+        font = style.font
+        font.name = 'Arial'
+        font.size = Pt(91)
+        document.add_paragraph(numList)
+        document.save("Drukowanie numerków.docx")
+
+    except ValueError as ve:
+        exit_program(0)
+
+def exit_program():
+    sys.exit(0)
 
 number_print_function()
