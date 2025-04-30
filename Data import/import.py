d03d1b60b0ed9c259bed99ab2fb7e6230c4f027f
pajak-sebastian <pajak.sebastian12@gmail.com>
2025-04-30 17:52:07 +0200

    added needed document styles

--- Data import/import.py
+++ Data import/import.py
@@ -1,10 +1,13 @@
 #pip install python
 #pip install python-docx
-#92px Cambria
+#92px Arial
+#dodać funkcjonalność która ogranicza dodanie zakresu ponad 100 liczb
+
 from docx import Document
+from docx.shared import Pt
 
 def number_print_function():
-    print("Podaj zakres")
+    print("Podaj zakres (maksymalnie do 100)")
 
     start = int(input("Od: "))
     end = int(input("Do: "))
@@ -12,6 +15,10 @@ def number_print_function():
     numList = ' '.join(str(i) for i in range(start, end + 1))
 
     document = Document()
+    style = document.styles["Normal"]
+    font = style.font
+    font.name = 'Arial'
+    font.size = Pt(91)
     document.add_paragraph(numList)
     document.save("Drukowanie numerków.docx")
 
